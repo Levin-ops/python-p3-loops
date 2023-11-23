e750b3f597b7d098f3d06fdce7f364754aa9ff7c
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-11-23 13:20:34 +0300

    Assignment Completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count > 0:
+        print(count)
+        count-=1
+    print("Happy New Year!")
 
+    
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [i ** 2 for i in int_list]
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    out_put = ""
 
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            out_put += "FizzBuzz\n"
+        elif i % 3 == 0:
+            out_put += "Fizz\n"
+        elif i % 5 == 0:
+            out_put += "Buzz\n"
+        else:
+            out_put += str(i) + "\n"
+
+    print (out_put)
+
+# fizzbuzz()
 # code along
 # while loop
 # i = 0
