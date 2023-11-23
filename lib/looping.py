644a3d8cbe240626c0a1a43a977dd050ea983921
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-11-23 12:27:16 +0300

    code along

--- lib/looping.py
+++ lib/looping.py
@@ -11,3 +11,33 @@ def square_integers(int_list):
 def fizzbuzz():
     # code goes here!
     pass
+
+# code along
+# while loop
+# i = 0
+# while i < 5:
+#     print("Looping!")
+#     i += 1
+
+# for i in range(10):
+#     print("Looping!")
+#     print(f"I am loop number:{i}")
+
+
+# player_heights = [0.008, 0.008, 0.008, 0.009, 0.008, 0.01, 0.009, 0.009, 0.01, 0.008, 0.009, 0.009, 0.008, 0.008, 0.008, 0.009, 0.008, 0.009, 0.01, 0.01]
+
+# before list comprehension
+# inch_height = list()
+# for height in player_heights:
+#     inches_height = height * 7920
+#     inch_height.append(inches_height)
+    
+# print(inch_height)
+
+# After list comprehension
+
+# inch_heights = [height * 7920 for height in player_heights]
+# print(inch_heights)
+
+# player_heights =[height * 7920 for height in player_heights]
+# print(player_heights)
